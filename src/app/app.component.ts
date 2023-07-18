90893b1f8d197678d474fcb16297dd7b163cea15
Piotr Bankiewicz <piotr.bankiewicz@yahoo.com>
2023-07-18 21:47:53 +0200

    Basic implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'NataliaiMarcin';
+  selectedFiles: FileList | undefined;
+  wishes: string | undefined;
+
+  constructor(private http: HttpClient) {}
+
+  onFileChange(event:any): void {
+    this.selectedFiles = event.target.files;
+  }
+
+  submitForm(f: NgForm): void {
+    const formData = new FormData();
+    if(this.selectedFiles){
+      for (let i = 0; i < this.selectedFiles.length; i++) {
+        formData.append('photos', this.selectedFiles[i]);
+      }
+
+    formData.append('wishes', f.value.wishes);
+
+      this.http.post('https://tasty-overshirt-jay.cyclic.app/api/upload', formData).subscribe(response => {
+        console.log(response);
+      });;
+    }
+  }
 }
